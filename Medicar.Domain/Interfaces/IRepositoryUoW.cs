bf4d6086181f224b3fb54373a527c263d59f711b
Renato Costa <renatosilvacosta1998@gmail.com>
2023-08-26 16:31:05 -0300

    repository UoW

---
+++ Medicar.Domain/Interfaces/IRepositoryUoW.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medicar.Domain.Interfaces
+{
+    public interface IRepositoryUoW : IDisposable
+    {
+        IDoctorRepository Doctors { get; }
+        ISpecialtyRepository Specialtys { get; } 
+
+        void Commit();
+        void Rowback();
+    }
+}
---
+++ Medicar.Infrastructure/Repositories/RepositoryUoW.cs
@@ -0,0 +1,42 @@
+﻿using Medicar.Domain.Interfaces;
+using Medicar.Infrastructure.Contexs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medicar.Infrastructure.Repositories
+{
+    public class RepositoryUoW : IRepositoryUoW
+    {
+        private ApplicationDbContext _dbContext;
+        private IDoctorRepository _doctorRepository;
+        private ISpecialtyRepository _specialtyRepository;
+
+        public RepositoryUoW(ApplicationDbContext context)
+        {
+            _dbContext = context;
+            _doctorRepository = new DoctorRepository(context);
+            _specialtyRepository = new SpecialtyRepository(context);
+        }
+        public IDoctorRepository Doctors => _doctorRepository;
+
+        public ISpecialtyRepository Specialtys => _specialtyRepository;
+
+        public void Commit()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Rowback()
+        {
+            
+        }
+    }
+}
