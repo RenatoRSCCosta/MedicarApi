f6d8c56153452a288e1d5d1393d1f60f6395f41d
Renato Costa <renatosilvacosta1998@gmail.com>
2023-08-26 15:55:42 -0300

    doctor and specialty repositories

---
+++ Medicar.Infrastructure/Repositories/DoctorRepository.cs
@@ -0,0 +1,46 @@
+﻿using Medicar.Domain.Interfaces;
+using Medicar.Infrastructure.Contexs;
+using Medicar_API.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Medicar.Infrastructure.Repositories;
+
+public class DoctorRepository : IDoctorRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+    public DoctorRepository(ApplicationDbContext context)
+    {
+        _dbContext = context;
+    }
+
+    public async Task<Doctor> CreateDoctor(Doctor doctor)
+    {
+        _dbContext.Doctors.Add(doctor);
+        await _dbContext.SaveChangesAsync();
+        return doctor;
+    }
+
+    public async Task DeleteDoctor(Doctor doctor)
+    {
+        _dbContext.Doctors.Remove(doctor);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<List<Doctor>> GetAllDoctors()
+    {
+        return await _dbContext.Doctors.ToListAsync();
+    }
+
+    public async Task<Doctor> GetDoctorById(int id)
+    {
+        return await _dbContext.Doctors.FindAsync(id);
+    }
+
+    public async Task<Doctor> UpdateDoctor(Doctor doctor)
+    {
+        _dbContext.Doctors.Update(doctor);
+        await _dbContext.SaveChangesAsync();
+        return await GetDoctorById(doctor.DoctorId);
+    }
+}
---
+++ Medicar.Infrastructure/Repositories/SpecialtyRepository.cs
@@ -0,0 +1,44 @@
+﻿using Medicar.Domain.Interfaces;
+using Medicar.Infrastructure.Contexs;
+using Medicar_API.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Medicar.Infrastructure.Repositories;
+
+public class SpecialtyRepository : ISpecialtyRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+    public SpecialtyRepository(ApplicationDbContext context)
+    {
+        _dbContext = context;
+    }
+    public async Task<Specialty> CreateSpecialty(Specialty specialty)
+    {
+        _dbContext.Specialtys.Add(specialty);
+        await _dbContext.SaveChangesAsync();
+        return specialty;
+    }
+
+    public async Task DeleteSpecialty(Specialty specialty)
+    {
+        _dbContext.Specialtys.Remove(specialty);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public Task<List<Specialty>> GetAllSpecialty()
+    {
+        return _dbContext.Specialtys.ToListAsync();
+    }
+
+    public async Task<Specialty> GetSpecialtyById(int id)
+    {
+        return await _dbContext.Specialtys.FindAsync(id);
+    }
+
+    public async Task<Specialty> UpdateSpecialty(Specialty specialty)
+    {
+        _dbContext.Specialtys.Update(specialty);
+        await _dbContext.SaveChangesAsync();
+        return await GetSpecialtyById(specialty.SpecialtyId);
+    }
+}
