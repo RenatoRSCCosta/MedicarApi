c5d498780858aff7513d2bc22d97359c2ee0d5d9
Renato Costa <renatosilvacosta1998@gmail.com>
2023-08-26 15:56:21 -0300

    add automapper

---
+++ Medicar.Application/Mappings/AutoMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Medicar.Application.Dtos;
+using Medicar_API.Domain.Entities;
+
+namespace Medicar.Application.Mappings;
+
+public class AutoMapping : Profile
+{
+    public AutoMapping() 
+    {
+        CreateMap<Doctor, DoctorDto>().ReverseMap();
+        CreateMap<Specialty, SpecialtyDto>().ReverseMap();
+    }
+}
