8416f41a1ff988ca14b459ed0049902a6729e574
Renato Costa <renatosilvacosta1998@gmail.com>
2023-08-26 15:57:08 -0300

    doctor and specialty dtos

---
+++ Medicar.Application/Dtos/DoctorDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medicar.Application.Dtos;
+
+public class DoctorDto
+{
+    public int DoctorId { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Crm { get; set; }
+    public string Email { get; set; }
+    [Required]
+    public int SpecialtyId { get; set; }
+    public SpecialtyDto Specialty { get; set; }
+
+}
---
+++ Medicar.Application/Dtos/SpecialtyDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medicar.Application.Dtos;
+
+public class SpecialtyDto
+{
+    [Required]
+    public int SpecialtyId { get; set; }
+    [Required]
+    public string Name { get; set; }
+}
