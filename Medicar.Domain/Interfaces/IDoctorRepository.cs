fe4f5efa4e6a8d0c61e3f2bdc35b08723b579898
Renato Costa <renatosilvacosta1998@gmail.com>
2023-08-26 15:54:45 -0300

    interfaces repositoy

---
+++ Medicar.Domain/Interfaces/IDoctorRepository.cs
@@ -0,0 +1,12 @@
+﻿using Medicar_API.Domain.Entities;
+
+namespace Medicar.Domain.Interfaces;
+
+public interface IDoctorRepository
+{
+    Task<List<Doctor>> GetAllDoctors();
+    Task<Doctor> GetDoctorById(int id);
+    Task<Doctor> CreateDoctor(Doctor doctor);
+    Task<Doctor> UpdateDoctor(Doctor doctor);
+    Task DeleteDoctor(Doctor doctor);
+}
---
+++ Medicar.Domain/Interfaces/ISpecialtyRepository.cs
@@ -0,0 +1,12 @@
+﻿using Medicar_API.Domain.Entities;
+
+namespace Medicar.Domain.Interfaces;
+
+public interface ISpecialtyRepository
+{
+    Task<List<Specialty>> GetAllSpecialty();
+    Task<Specialty> GetSpecialtyById(int id);
+    Task<Specialty> CreateSpecialty(Specialty specialty);
+    Task<Specialty> UpdateSpecialty(Specialty specialty);
+    Task DeleteSpecialty(Specialty specialty);
+}
